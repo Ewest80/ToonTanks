3e9b38146d3b090dfdce7b6622c834c45f03e219
Eric <2019900+Ewest80@users.noreply.github.com>
2022-01-05 00:51:45 -0500

    Lesson 153 Timers

--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -26,4 +26,9 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 1000.f;
 
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+
+	bool InFireRange();
 };
